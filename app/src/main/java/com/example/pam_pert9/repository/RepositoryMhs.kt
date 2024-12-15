057d620798f5abcc1f04b99190c9aa88762e3551
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-15 20:19:59 +0700

    ADD - method untuk memanggil method read semua data yang telah di buat di dao

--- app/src/main/java/com/example/pam_pert9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pam_pert9/repository/RepositoryMhs.kt
@@ -1,6 +1,7 @@
 package com.example.pam_pert9.repository
 
 import com.example.pam_pert9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
@@ -8,5 +9,7 @@ interface RepositoryMhs {
     suspend fun deleteMhs(mahasiswa: Mahasiswa)
 
     suspend fun updateMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 
 }
