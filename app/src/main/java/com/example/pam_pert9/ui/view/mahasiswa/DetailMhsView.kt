91db831bf32c1579b4b9a309626aa2c19dd98066
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-15 21:47:54 +0700

    ADD - confirmation dialog untuk delete data

--- app/src/main/java/com/example/pam_pert9/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pam_pert9/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,8 +1,35 @@
 package com.example.pam_pert9.ui.view.mahasiswa
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun DetailMhsView(){
 
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = {},
+        title = { Text("Delete Data")},
+        text = { Text("Apakah anda yakin ingin menghapus data?")},
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
 }
